b826d5f09fe3d7a3612ad1afa990a3a796e53926
Cali Mitchell <47833766+caliew26@users.noreply.github.com>
2019-08-22 04:04:43 -0400

    Update Detail.js

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -20,11 +20,11 @@ class Detail extends Component {
         <div id="map">
           <h2>We are here</h2>
           {/* <img src="layneandmarlisbuilding.jpg" className="buildingPeas" alt="Layne and Marlis Building"></img> */}
-          <img src="https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=AIzaSyDAllIaQbeIUae7GUfKRWPhPaRQ3xVpFn8" alt="google map"></img>
+          <img src="https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=" alt="google map"></img>
           {/* <img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
           &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
           &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-          &key=AIzaSyDAllIaQbeIUae7GUfKRWPhPaRQ3xVpFn8" alt="google map"></img> */}
+          &key=" alt="google map"></img> */}
         </div>
         <h1>Here are items we could have for sale:</h1>
         {this.state.products.map(product =>  (
