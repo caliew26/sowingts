289ee2530e50088c0ed18cea56c87811bd5e3010
Adventure 2019 <caliew26@gmail.com>
2019-08-29 10:10:50 -0400

    uncommented the const PORT under server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysql = require("mysql2")
 const routes = require("./routes");
 const app = express();
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const db = require("./models");
 require('dotenv').config();
 
